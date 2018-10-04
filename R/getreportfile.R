b90b2c8b80156ad89979647c5023c117bea98c6c
Daphnis De Pooter <Daphnisd@vliz.be>
2018-10-04 14:41:27 +0200

    Update getreportfile.R

--- R/getreportfile.R
+++ R/getreportfile.R
@@ -1,6 +1,6 @@
 
  download.file("https://raw.githubusercontent.com/EMODnet/LifeWatch-EMODnet-Biology-QC-tool/master/R/reportfile.Rmd",
                 "reportfile.Rmd")
- download.file("https://raw.githubusercontent.com/EMODnet/LifeWatch-EMODnet-Biology-QC-tool/master/R/README.md",
+ download.file("https://raw.githubusercontent.com/EMODnet/LifeWatch-EMODnet-Biology-QC-tool/master/README.md",
                 "about.md")
 
