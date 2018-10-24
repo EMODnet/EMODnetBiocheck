87d8869285ec7c1f16ac6a53d2f6b6ed92cf97a1
Daphnis De Pooter <daphnisd@vliz.be>
2018-10-24 12:05:18 +0200

    added linux dependancies

--- R/installallneededpackages.r
+++ R/installallneededpackages.r
@@ -13,3 +13,12 @@ install.packages("knitr")
 install.packages("leaflet")
 install.packages("stringr")
 
+
+
+
+# For use in Linux you will need to have installed the folowing Linux libraries
+### libgeos-dev
+### rgdal-dev
+### librgdal-dev
+### libgdal-dev
+### libudunits2-dev
