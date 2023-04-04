341da71cb190ea2c881b0bcc06851822c9d9885d
LynnDelgat <83461360+LynnDelgat@users.noreply.github.com>
2023-04-04 15:39:08 +0200

    Allow for files

--- R/importiptdata.R
+++ R/importiptdata.R
@@ -25,7 +25,7 @@ importiptdata <- function (file){
      file <- gsub("manage/", "", file) }
   
   
-  if ( ((grepl("archive?", file)==FALSE | url.exists(file) == FALSE & valid_url(file) == FALSE)) & grepl(".zip", file) == FALSE){
+  if (grepl(".zip", file) == FALSE && ((grepl("archive?", file)==FALSE | url.exists(file) == FALSE & valid_url(file) == FALSE))){
     output$error <- ("Link does not resolve to a public IPT resource")
   }  else   {
   
